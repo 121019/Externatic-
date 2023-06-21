6c6bfec66b1986a9b83f7bea8f0d412e924829c5
Wilhem-H <wilhem.hafsa@outlook.fr>
2023-06-21 16:19:28 +0200

    add burger menu in mobile frame

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import ExternaticLogoSansFond from "../assets/ExternaticLogoSansFond.png";
 import "./Navbar.css";
@@ -11,15 +11,80 @@ function Navbar() {
     { path: "/contact", text: "Nous contacter" },
   ];
 
+  const [openMenu, setOpenMenu] = useState("false");
+
+  const openMenuMobile = () => {
+    setOpenMenu((value) => !value);
+  };
+
   return (
     <div>
+      <div
+        className={
+          openMenu ? "overlay_menu_mobile" : "overlay_menu_mobile open"
+        }
+      >
+        {/* {" "} Intentionally left empty {" "} */}
+      </div>
       <nav className="navbar_nav">
+        <div
+          className="navbar_burger"
+          onClick={openMenuMobile}
+          onKeyDown={(event) => {
+            if (event.key === "'Enter' || event.key === ' '") {
+              openMenuMobile();
+            }
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
         <img
           className="logoExternatic"
           src={ExternaticLogoSansFond}
           alt="logo entreprise externatic"
         />
-        <div className="navbar">
+        <div className={openMenu ? "navbar" : "navbar open"}>
+          <div
+            className="navbar_burger_close"
+            onClick={openMenuMobile}
+            onKeyDown={(event) => {
+              if (event.key === "'Enter' || event.key === ' '") {
+                openMenuMobile();
+              }
+            }}
+            role="button"
+            tabIndex={0}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
           <div className="links">
             {links.map((link) => (
               <div key={link.text}>
