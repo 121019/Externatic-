1204ff0aaf5da0b56f5d851426515a50a1a0d6ea
julien-lad <julien.ladreyt@gmail.com>
2023-07-05 13:39:13 +0200

    début de création du contenu de la page espace candidat

--- frontend/src/Navbar/MonEspace.jsx
+++ frontend/src/Navbar/MonEspace.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./MonEspace.css";
 
 function MonEspace() {
-  return <div>{/*  code */}</div>;
+  return (
+    <div>
+      <div className="monEspace">
+        <h3>Mon espace</h3>
+      </div>
+      <h4 className="monEspace_nomcomplet-h4"> Homer Simpson</h4>
+    </div>
+  );
 }
 
 export default MonEspace;
