9aa0d0714a1ab75851d601877984a02965cd906b
Nils <nils.mehlhorn.fr@gmail.com>
2023-07-20 12:22:14 +0200

    validation insertCompany backend data

--- backend/src/models/CompanyManager.js
+++ backend/src/models/CompanyManager.js
@@ -18,19 +18,46 @@ class CompanyManager extends AbstractManager {
     );
   }
 
-  insert(company) {
-    if (!company.id || typeof company.id !== "number") {
-      throw new Error("Invalid or missing 'id' in the company data.");
-    }
-
+  async insert(company) {
     if (!company.name || typeof company.name !== "string") {
       throw new Error("Invalid or missing 'name' in the company data.");
     }
-    if (!company.email || typeof company.email !== "string") {
-      throw new Error("invalide or missing 'email' in the company data");
+
+    const existingCompany = await this.database.query(
+      `SELECT name FROM ${this.table} WHERE name = ?`,
+      [company.name]
+    );
+
+    if (existingCompany.length > 0) {
+      throw new Error("Company with this 'name' already exists.");
     }
+
+    if (!company.email || typeof company.email !== "string") {
+      throw new Error("Invalid or missing 'email' in the company data.");
+    }
+    if (!company.hashedpassword || typeof company.hashedpassword !== "string") {
+      throw new Error(
+        "Invalid or missing 'hashedpassword' in the company data."
+      );
+    }
+    if (!company.description || typeof company.description !== "string") {
+      throw new Error("Invalid or missing 'description' in the company data.");
+    }
+    if (!company.address || typeof company.address !== "string") {
+      throw new Error("Invalid or missing 'address' in the company data.");
+    }
+    if (!company.city || typeof company.city !== "string") {
+      throw new Error("Invalid or missing 'city' in the company data.");
+    }
+    if (
+      !company.postcode ||
+      !Number.isInteger(parseInt(company.postcode, 10))
+    ) {
+      throw new Error("Invalid or missing 'postcode' in the company data.");
+    }
+
     return this.database.query(
-      `INSERT INTO ${this.table} (id, name, email,hashedpassword, description, address, city, postcode) VALUES (?,?,?,?,?,?,?,?)`,
+      `INSERT INTO ${this.table} (id, name, email, hashedpassword, description, address, city, postcode) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
       [
         company.id,
         company.name,
